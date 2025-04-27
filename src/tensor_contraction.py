Fixed errors in cost calculation, minor refactoring

src/tensor_contraction.py
@@ -5,58 +5,96 @@ import matplotlib.pyplot as plt
 
 class Node:
 
-    def __init__(self, out_dims):
-        self.out_dims = np.array(out_dims, dtype=int)
+    def __init__(self, links, dim_ranges):
+        '''
+        links[i] is index of neighbor of node connected to i-th dimension
+        dim_ranges[i] is size of dimension i
+        '''
+        self.links = np.array(links, dtype=int)
+        self.dim_ranges = np.array(dim_ranges)
 
     def reassign_links(self, old, new):
-        self.out_dims[self.out_dims == old] = new
-        
+        '''
+        If any connections to node with index 'old', redirect to index 'new'
+        '''
+        self.links[self.links == old] = new
+
 
 class TensorNetwork:
 
     def __init__(self, nodes: list[Node]):
         self.nodes = nodes
-        self.active = np.full(len(nodes), True, dtype=bool)
+        self.active = np.full(len(self), True, dtype=bool)
+
+    def __len__(self) -> int:
+        return len(self.nodes)
 
     def links_of(self, i):
-        return self.nodes[i].out_dims
+        return self.nodes[i].links
 
-    def merge_nodes(self, i: int, j: int) -> int:
+    def ranges_of(self, i):
+        return self.nodes[i].dim_ranges
 
-        if i >= len(self.nodes) or j >= len(self.nodes):
+    def mutual_ranges(self, i, j):
+        '''
+        Return sizes of dimensions to be merged with node j
+        (or empty array, if no such dimensions)
+        '''
+        return self.ranges_of(i)[self.links_of(i) == j]
+
+    def validate_merge(self, i: int, j: int) -> None:
+
+        if i == j:
+            raise ValueError(f"Attempt to merge node {i} with itself")
+
+        if i >= len(self) or j >= len(self):
             raise ValueError(
                 f"Cannot contract: node {i} or {j} not in network")
 
         if not self.active[i] or not self.active[j]:
+            raise ValueError(f"Either {i} or {j} is inactive")
+
+        if self.mutual_ranges(i, j) != self.mutual_ranges(j, i):
             raise ValueError(
-                f"Either {i} or {j} is inactive")
+                f"Cannot merge nodes {i}, {j} over dims of different size")
 
-        if i == j:
-            return 0
+    def merge_cost(self, i,  j):
+        '''
+        Cost of merge, including case of no common indices
+        '''
+        cost = np.prod(self.ranges_of(i)) * np.prod(self.ranges_of(j))
+        mutual_ranges = self.mutual_ranges(i, j)
+        if mutual_ranges.size != 0:
+            cost /= np.prod(mutual_ranges)
+        return cost
 
-        x = self.links_of(i)
-        y = self.links_of(j)
-        n_mutual = np.sum(x == j)
-
-        if n_mutual != np.sum(y == i):
-            raise ValueError(f"Mismatch in links between nodes {i} and {j}")
-
-        cost = (len(x) - n_mutual) * (len(y) - n_mutual) * n_mutual
+    def merge_nodes(self, i: int, j: int) -> int:
+        '''
+        1) Merge nodes i and j
+        2) Save result at index j
+        3) Return cost of the merge
+        '''
+        self.validate_merge(i, j)
+        cost = self.merge_cost(i, j)
 
         # For every neighbor connected to node i,
         # replace their connection to i with a connection to j
-        for neighbor in x:
+        for neighbor in self.links_of(i):
             if neighbor not in [-1, j]:
                 self.nodes[neighbor].reassign_links(old=i, new=j)
 
-        self.nodes[j].out_dims = np.concatenate([x[x != j], y[y != i]])
-
+        # Save result of merge to j according to rules of np.tensordot
+        x = self.links_of(i)
+        y = self.links_of(j)
+        rg_x = self.ranges_of(i)
+        rg_y = self.ranges_of(j)
+        self.nodes[j].links = np.concatenate([x[x != j], y[y != i]])
+        self.nodes[j].dim_ranges = np.concatenate(rg_x[x != i], rg_y[y != j])
         self.active[i] = False
-
         return cost
 
     def draw_network(self, title='Tensor Network'):
-        
+
         G = nx.MultiDiGraph()
         edge_index_map = {}  # (src, dst) -> list of indices
 
@@ -64,10 +102,10 @@ class TensorNetwork:
             if not self.active[i]:
                 continue
             G.add_node(i)
-            for j in node.out_dims:
+            for j in node.links:
                 if j != -1:
                     G.add_edge(i, j)
-                    edge_index_map[(i, j)] = np.where(self.links_of(i)==j)
+                    edge_index_map[(i, j)] = np.where(self.links_of(i) == j)
 
         pos = nx.spring_layout(G)
         nx.draw(G, pos, with_labels=True)
@@ -80,10 +118,7 @@ class TensorNetwork:
             alpha = 0.15  # position near the start of the edge
             x, y = (1 - alpha) * x1 + alpha * x2, (1 - alpha) * y1 + alpha * y2
             label = str(list(*indices))
-            ax.text(x, y, label, color = 'red')
+            ax.text(x, y, label, color='red')
 
         plt.title(title)
         plt.show()
-
-
-